a06c1f915638198bde6a9825d8268349cae9c0af
BalaciSofia <klara.balaci@stud.ubbcluj.ro>
2026-06-28 23:34:32 +0300

    set up Angular routing and app layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { Calendar } from './pages/calendar/calendar';
+import { Employees } from './pages/employees/employees';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { NewEmployee } from './pages/new-employee/new-employee';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'calendar',
+    component: Calendar,
+  },
+  {
+    path: 'profile',
+    component: Profile,
+  },
+  {
+    path: 'new-employee',
+    component: NewEmployee,
+  },
+  {
+    path: 'employees',
+    component: Employees,
+  },
+];
